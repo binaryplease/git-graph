a6ea26f33823f2ba801a75ec81b630a830ddbc80
Enrico Scherlies <e.scherlies@pm.me>
2026-08-15 19:41:54 +0200

    feat: add MIT license
    Nightshift-Task-Id: NST9265-add-mit-license

--- flake.nix
+++ flake.nix
@@ -132,6 +132,8 @@
 
             meta = {
               description = "Local git commit-graph viewer — browse your repositories' history in the browser";
+              homepage = "https://github.com/binaryplease/binp-git-graph";
+              license = pkgs.lib.licenses.mit;
               mainProgram = "bgg";
               platforms = pkgs.lib.platforms.unix;
             };
